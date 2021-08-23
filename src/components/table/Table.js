51b6233211c3fc435c6841ccf5d73b011aca1519
saberpourrahimi <saber.pourrahimi.1999@gmail.com>
2021-08-24 03:24:16 +0430

    add search view

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,11 +1,12 @@
-import { AD_NAME, DATE, FIELD, MODIFIER_NAME, NEW_VALUE, OLD_VALUE } from '../../constant/strings'
+import { AD_NAME, DATE, FIELD, MODIFIER_NAME, NEW_VALUE, OLD_VALUE, PRICE, TITLE } from '../../constant/strings'
 import './Table.css'
 import Data from "../../constant/data.json"
 import { useEffect, useState } from 'react'
 import useSortableData from '../../utils/useSortableData'
 
 function Table() {
-  const [listSize, setListSize] = useState(5)
+  const [listSize, setListSize] = useState(30)
+  console.log(listSize)
   const { items, requestSort, bookMarkList } = useSortableData(Data)
   const bookMarkm = JSON.parse(window.localStorage.getItem("book-mark")) || []
   
@@ -27,30 +28,53 @@ function Table() {
   },[listSize])
 
   return (
-    <table>
-      <thead>
-        <tr className="table-head">
-          <td onClick={() => requestSort('name')}>{MODIFIER_NAME} {URLsort === "name" && <Icon />}</td>
-          <td onClick={() => requestSort('date')}>{DATE} {URLsort === "date" && <Icon />}</td>
-          <td onClick={() => requestSort('title')}>{AD_NAME} {URLsort === "title" && <Icon />}</td>
-          <td onClick={() => requestSort('field')}>{FIELD} {URLsort === "field" && <Icon />}</td>
-          <td onClick={() => requestSort('old_value')}>{OLD_VALUE} {URLsort === "old_value" && <Icon />}</td>
-          <td onClick={() => requestSort('new_value')}>{NEW_VALUE} {URLsort === "new_value" && <Icon />}</td>
-        </tr>
-      </thead>
-      <tbody id="infinite-list">
-        {items.slice(0, listSize).map((item) => 
-          <tr onClick={() => bookMarkList(item.id)} key={item.id} className="table-body" style={{backgroundColor: bookMarkm.includes(item.id) && "yellow"}}>
-            <td>{item.name}</td>
-            <td>{new Date(item.date).toLocaleDateString("fa-IR")}</td>
-            <td>{item.title}</td>
-            <td>{item.field}</td>
-            <td>{item.old_value}</td>
-            <td>{item.new_value}</td>
+    <>
+      <div className="search">
+        <div>
+          <label>{MODIFIER_NAME}</label>
+          <input type="text" name={MODIFIER_NAME} />
+        </div>
+        <div>
+          <label>{DATE}</label>
+          <input type="date" name={DATE} />
+        </div>
+        <div>
+          <label>{AD_NAME}</label>
+          <input type="text" name={AD_NAME} />
+        </div>
+        <div>
+          <label>{FIELD}</label>
+          <select name={FIELD}>
+            <option value={TITLE}>{TITLE}</option>
+            <option value={PRICE}>{PRICE}</option>
+          </select>
+        </div>
+      </div>
+      <table className="table">
+        <thead>
+          <tr className="table-head">
+            <td onClick={() => requestSort('name')}>{MODIFIER_NAME} {URLsort === "name" && <Icon />}</td>
+            <td onClick={() => requestSort('date')}>{DATE} {URLsort === "date" && <Icon />}</td>
+            <td onClick={() => requestSort('title')}>{AD_NAME} {URLsort === "title" && <Icon />}</td>
+            <td onClick={() => requestSort('field')}>{FIELD} {URLsort === "field" && <Icon />}</td>
+            <td onClick={() => requestSort('old_value')}>{OLD_VALUE} {URLsort === "old_value" && <Icon />}</td>
+            <td onClick={() => requestSort('new_value')}>{NEW_VALUE} {URLsort === "new_value" && <Icon />}</td>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody id="infinite-list">
+          {items.slice(0, listSize).map((item) => 
+            <tr onClick={() => bookMarkList(item.id)} key={item.id} className="table-body" style={{backgroundColor: bookMarkm.includes(item.id) && "yellow"}}>
+              <td>{item.name}</td>
+              <td>{new Date(item.date).toLocaleDateString("fa-IR")}</td>
+              <td>{item.title}</td>
+              <td>{item.field}</td>
+              <td>{item.old_value}</td>
+              <td>{item.new_value}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
   )
 }
 
--- src/constant/strings.js
+++ src/constant/strings.js
@@ -4,3 +4,5 @@ export const AD_NAME = "نام آگهی";
 export const FIELD = "فیلد";
 export const OLD_VALUE = "مقدار قدیمی";
 export const NEW_VALUE = "مقدار جدید";
+export const TITLE = "عنوان";
+export const PRICE = "قیمت";
